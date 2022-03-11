5e3a2753636049091fda5bd1bc164136e47247e6
Justin <35445717+jlum07@users.noreply.github.com>
2022-03-10 22:05:25 -0500

    Getting api data and loading to sate

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function Nav() {
@@ -31,22 +32,57 @@ function TransactionsItem () {
   )
 }
 
-function Transactions () {
+function Transactions ({transactions}) {
   return (
     <table className='transactions-table'>
       <TransactionsHeader />
-      <TransactionsItem />
-      <TransactionsItem />
+      {transactions.map((transaction) => (
+        <TransactionsItem />
+      ))}
     </table>
   )
 }
 
+function Loading () {
+  return (
+    <div className='message'>Loading...</div>
+  )
+}
+
 function App() {
+  
+  const [transactions, setTransactions] = useState(false);
+  
+  useEffect(()=> {
+    setTransactions(fetchTransactions())
+  }, [])
+
+  console.log(transactions)
+  async function fetchTransactions() {
+    let allTransactions = [];
+    let page = 1;
+    let morePages = true;
+    
+    while (morePages) {
+      let response = await fetch(`https://resttest.bench.co/transactions/${page}.json`);
+      let { totalCount, transactions } = await response.json();
+      allTransactions = allTransactions.concat(transactions);
+      page++;
+      morePages = allTransactions.length < totalCount;
+    }
+    
+    setTransactions(allTransactions);
+  }
 
   return (
     <div className="App">
       <Nav />
-      <Transactions />
+      {transactions ? 
+        <Transactions transactions={transactions} />
+      :
+        <Loading />
+      }
+      
     </div>
   );
 }
